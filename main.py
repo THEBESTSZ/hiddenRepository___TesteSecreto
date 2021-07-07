045b4ada4d75cb9d819210b5d9e00ad6bfde6c41
Will <william.felipe@ufms.br>
2021-07-07 00:54:59 -0300

    finalizando leitura em excel

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from pathlib import Path
 
-filedir = os.path.join(Path().absolute(), "dataset_small")
+filedir = os.path.join(Path().absolute())
 filename = "general_ledger.xlsx"
 
 def reader(): 
@@ -15,13 +15,19 @@ ledger = reader()
 
 chart = {}
 for key in list(ledger):
+	print(key)
 	chart[key] = ledger[key]
 	k = key.rfind(".")
 	while(k > 0):
-		ledgerFather = key[:k]
-		print(ledgerFather)
-		k = ledgerFather.rfind(".")
-	#print(key)
+		ancestor = key[:k]
+		if ancestor in ledger:
+			chart[ancestor] = chart[ancestor] + ledger[key]
+		else:
+			chart[ancestor] = ledger[key]
+		print(ancestor)		
+		k = ancestor.rfind(".")
+	print(chart)
+	break
 	removido = ledger.pop(key)
 	#print(removido)
 	#print(key)
