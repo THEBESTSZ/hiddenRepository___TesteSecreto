521a39c66a62873d50ce8e969f3a9f8a90ced0a0
Will <william.felipe@ufms.br>
2021-07-07 23:36:39 -0300

    optado por utilizar tuplas

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ fileReturn = "chart_return.xlsx"
 
 def readerXlsx(): 
 	general_ledger = pd.read_excel(os.path.join(filedir, "input", filename))
-	general_dict = dict(zip(general_ledger.account, general_ledger.value))
-	return general_dict
+	# general_dict = dict(zip(general_ledger.account, general_ledger.value))
+	general_tuple = tuple(zip(general_ledger.account, general_ledger.value))
+	# print(general_tuple)
+	return general_tuple
 
 def chartReturnXlsx(chart):
-	df1 = pd.DataFrame(chart.items(), columns=['account', 'value']).sort_values(by = 'account')
+	df1 = pd.DataFrame(chart.items(), columns=['account', 'value']).sort_values(by = 'account').round(2)
 	if not os.path.exists(os.path.join(filedir, "output")):
 		os.makedirs(os.path.join(filedir, "output"))
 	with pd.ExcelWriter(os.path.join(filedir, "output","path_to_file.xlsx")) as writer:
 		df1.to_excel(writer, sheet_name="Sheet1", index=False)
-	print(df1)
+	#print(df1)
 
 def createSql(): 
 	banco = sqlite3.connect('deepesg.sql')
@@ -31,7 +33,7 @@ def readerSql():
 	banco = sqlite3.connect('deepesg.sql')
 	cursor = banco.cursor()
 	cursor.execute("SELECT * FROM general_ledger")
-	general_dict = dict(cursor.fetchall())
+	general_dict = tuple(cursor.fetchall())
 	print(general_dict)
 
 def insertSql():
@@ -43,27 +45,29 @@ def insertSql():
 ledger = readerXlsx()
 
 chart = {}
-for key in list(ledger):
+for element in ledger:
+	key = element[0]
+	value = element[1]
+
 	print(key)
+	print(value)
 	if key in chart:
-		chart[key] = chart[key] + ledger[key]
+		chart[key] = chart[key] + value
 	else:
-		chart[key] = ledger[key]
+		chart[key] = value
 	k = key.rfind(".")
 	while(k > 0):
 		ancestor = key[:k]
 		if ancestor in chart:
-			chart[ancestor] = chart[ancestor] + ledger[key]
+			chart[ancestor] = chart[ancestor] + value
 		else:
-			chart[ancestor] = ledger[key]
-		print(ancestor)		
+			chart[ancestor] = value
 		k = ancestor.rfind(".")
-	removido = ledger.pop(key)
 # print(chart)
 
 chartReturnXlsx(chart)
 
-insertSql()
+# insertSql()
 
 readerSql()
 
